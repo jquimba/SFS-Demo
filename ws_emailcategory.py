ccf7374be058639fb73bdbd374076930724bcaf4
jquimba <julian.quimbayo@vass.es>
2024-05-03 12:28:03 +0200

    add ws

--- ws_emailcategory.py
+++ ws_emailcategory.py
@@ -1,4 +1,10 @@
 import os
+from flask import Flask
 
-port = int(os.environ.get('PORT', 5000))
-print("El puerto de escucha es:", port)
+app = Flask(__name__)
+@app.route('/')
+def hello():
+        return '¡Hola, mundo! Este es un servicio web en Python desplegado en Heroku.'
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    print("El puerto de escucha es:", port)
