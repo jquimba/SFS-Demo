eed061dc43afa9e3d63fbbf556eb8dd0e058fac5
jquimba <julian.quimbayo@vass.es>
2024-05-03 12:50:30 +0200

    add port

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    port = int(os.environ.get('PORT', 5000))
+    print("El puerto de escucha es:", port)
     return "Hello, Flask!"
