3b03e1cf096f4a3a8c77f96aebb1436064468c88
jquimba <julian.quimbayo@vass.es>
2024-05-03 12:52:38 +0200

    test port

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    return "Hello, Flask!"
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     print("El puerto de escucha es:", port)
-    return "Hello, Flask!"
