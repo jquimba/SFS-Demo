e549e40271047d75ce27dea73852a7a48c0eb4bc
jquimba <julian.quimbayo@vass.es>
2024-05-03 11:47:11 +0200

    test

--- ws_emailcategory.py
+++ ws_emailcategory.py
@@ -1,2 +1,10 @@
-mensaje = "Hola, mundo!"
-print(mensaje)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '¡Hola, mundo! Este es un servicio web en Python desplegado en Heroku.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
