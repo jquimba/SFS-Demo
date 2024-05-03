62fa377c7ccb70fa0d688330a6fed8abd6618306
jquimba <julian.quimbayo@vass.es>
2024-05-03 11:44:08 +0200

    test

--- ws_emailcategory.py
+++ ws_emailcategory.py
@@ -1,10 +1,2 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return '¡Hola, mundo! Este es un servicio web en Python.'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+mensaje = "Hola, mundo!"
+print(mensaje)
