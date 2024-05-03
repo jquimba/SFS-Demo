b6ed78159c694705d5cb89f76da59c57c4dc351d
jquimba <julian.quimbayo@vass.es>
2024-05-03 12:23:14 +0200

    test

--- ws_emailcategory.py
+++ ws_emailcategory.py
@@ -1,13 +1,4 @@
-from flask import Flask
 import os
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return '¡Hola, mundo! Este es un servicio web en Python desplegado en Heroku.'
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    port = int(os.environ.get("PORT", 80))
-    app.run(host='0.0.0.0', port=port)
+port = int(os.environ.get('PORT', 5000))
+print("El puerto de escucha es:", port)
