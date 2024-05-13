6133a07a42cfa360343aef19283148a9885a1b70
jquimba <julian.quimbayo@vass.es>
2024-05-13 11:20:16 +0200

    update

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-from transformers import AutoTokenizer, TFBertForSequenceClassification
-import tensorflow as tf
-
-tokenizer = AutoTokenizer.from_pretrained("ydshieh/bert-base-uncased-yelp-polarity")
-model = TFBertForSequenceClassification.from_pretrained("ydshieh/bert-base-uncased-yelp-polarity")
-
-inputs = tokenizer("Hello, my dog is cute", return_tensors="tf")
-
-logits = model(**inputs).logits
-
-predicted_class_id = int(tf.math.argmax(logits, axis=-1)[0])
-model.config.id2label[predicted_class_id]
+# Load model directly
+from transformers import AutoModel
+model = AutoModel.from_pretrained("huawei-noah/TinyBERT_General_6L_768D")
